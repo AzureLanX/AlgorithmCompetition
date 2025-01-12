dcf587979e3ab46683111bf6737a283a98e77444
AzureLanX <yuhaolan311@gmail.com>
2025-01-12 17:14:51 +0800

    init commit

---
+++ src/哈希表/哈市/Main.java
@@ -0,0 +1,15 @@
+package 哈希表.哈市;
+
+/**
+ * @BelongsProject: AlgorithmCompetition
+ * @BelongsPackage: 哈希表.哈市
+ * @Author: JuferBlue
+ * @CreateTime: 2025-01-12  17:07
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
